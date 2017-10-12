ad783f3addbb0f91223a064bc6b2a69619c23ea8
Carlos Abad Sánchez <cabad@alumni.afi.es>
2017-10-13 00:34:10 +0200

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,25 @@
+#' hola
+#'
+#' Say hello to the world.
+#'
+#' @return String with your greetings.
+#' @examples
+#' hola()
+#' @export
+hola <- function() {
+  return("Hello, world!")
+}
+
+#' suma
+#'
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y Another number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(2, 3)
+#' @export
+suma <- function(x, y) {
+  x + y
+}
